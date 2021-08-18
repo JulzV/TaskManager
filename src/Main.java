3d4812972896a2294c74e8371f5c72c320cbdf73
JulzV <volkova.julija@inbox.lv>
2021-08-18 21:23:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+//    TASK:
+/*  1. User login
+        1.	create user
+        2.	delete user
+2. Choose action:
+            1.	add new task;
+            2.	add task priority (1 till 3 -> user can add new priority);
+            3.	edit task;
+            4.	delete task (are you sure?) - Kristīne;
+            5.	deadline (date);
+            6.	open existing tasks (by statuss: have to do; in progress; done - > user can add new category);
+            7.	archivate task- Kristīne;
+            8.	clear archive (all data will be deleted. No restore possible. Are you sure?) - Kristīne;
+
+    Extra:
+            1.	sort by statuss?
+            2.	day counnt till deadline?
+            3.	Add new columns/ categories??
+
+*/
+
+
+}
